d7bb7d40f67036daa0e5330e73c527a2b9561d47
feiiit <lagzinsta@gmail.com>
2024-07-15 00:17:20 +0300

    bandau padaryt kad cloudflare persijungtu ant main

--- js/list.js
+++ js/list.js
@@ -24,7 +24,7 @@ const d = {
       "name": "undisclosed",
       "author": "ArcticEggz [yolomodeboom]",
       "more": "none",
-      "id": 83775625,
+      "id": 83775635,
       "pass": "Free to copy",
       "percentToQualify": 100,
       "verificationVid": "https://youtu.be/OcP7QFZtChs",
