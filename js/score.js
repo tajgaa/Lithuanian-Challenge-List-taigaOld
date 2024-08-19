37b194fa68b8d00016ee5a4d155a188f7c2becc1
KandyMun <rektelkid@gmail.com>
2024-08-19 14:30:27 +0300

    Reworked the points calculation system, hopefully removing all spots where level point difference is way less than it should be.

--- js/score.js
+++ js/score.js
@@ -1,26 +1,45 @@
 export function score(rank){
-  var top10Score = 250/(Math.log(3 * rank + 10) / Math.log(13));
-  var top11_50Score = (Math.sin(0.08 * (rank + 8.905))*70) + 100;
-  var top51_75Score = 1.83 + (1/(0.0036*(rank-41)))
+  // var top10Score = 250/(Math.log(3 * rank + 10) / Math.log(13));
+  // var top11_50Score = (Math.sin(0.08 * (rank + 8.905))*70) + 100;
+  // var top51_75Score = 1.83 + (1/(0.0036*(rank-41)))
 
-  if(rank => 1){ //rank has to be above 1
-    if(rank < 11)//if rank is 10 or above
-    { 
-      return round(top10Score);
+  // if(rank => 1){ //rank has to be above 1
+  //   if(rank < 11)//if rank is 10 or above
+  //   { 
+  //     return round(top10Score);
+  //   }
+  //   else if(rank > 10 & rank < 51)//if rank is between 11 and 50
+  //   { 
+  //     return round(top11_50Score)
+  //   }
+  //   else if(rank > 50 && rank < 76)//if rank is between 51 and 75
+  //   { 
+  //     return round(top51_75Score);
+  //   }
+  //   else //return 0 if rank goes beyond 75 (it counts as legacy)
+  //   {
+  //     return 0;
+  //   }
+  // }
+
+  //var top10Score = 149.61*(1.137^(1-rank))+100.39;
+  //var top11_35Score = 166.611*(1.0099685^(17-rank))-31.152;
+  //var top36_55Score = 212.61*(1.036^(14-rank))+6.071;
+  //var top56_75Score = 56.191*(2^((78.147-(rank+3.2))((Math.log(50)/99))))-41.10949;
+  if (rank >= 1){
+    if(rank < 11){
+      return RoundPoints(149.61*(1.137^(1-rank))+100.39);
     }
-    else if(rank > 10 & rank < 51)//if rank is between 11 and 50
-    { 
-      return round(top11_50Score)
+    else if(rank > 10 & rank <= 35){
+      return RoundPoints(166.611*(1.0099685^(17-rank))-31.152);
     }
-    else if(rank > 50 && rank < 76)//if rank is between 51 and 75
-    { 
-      return round(top51_75Score);
+    else if (rank > 35 & rank <= 55){
+      return RoundPoints(212.61*(1.036^(14-rank))+6.071);
     }
-    else //return 0 if rank goes beyond 75 (it counts as legacy)
-    {
-      return 0;
+    else if (rank > 55 & rank <= 75){
+      return RoundPoints(56.191*(2^((78.147-(rank+3.2))((Math.log(50)/99))))-41.10949);
     }
-  }
+    else return 0;
 }
 
 const scale = 2; //Select how many decimal digits to round to
@@ -30,7 +49,7 @@ const scale = 2; //Select how many decimal digits to round to
  * @param {Number} scale    Number of decimal points to round to
  * @returns {Number}        Rounded number
  */
-export function round(num)
+export function RoundPoints(num)
 {
     return Math.round(num*(10**scale))/(10**scale);
 }
