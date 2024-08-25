7b11a4f09356be4004da3b15dfbd4bf3d8d909b4
KandyMun <rektelkid@gmail.com>
2024-08-25 18:18:08 +0300

    fixes

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -62,7 +62,7 @@ export default {
                         <template v-if="entry.createdLevels.length > 0">
                             <h2>Sukurti challenge'ai ({{entry.createdLevels.length}})</h2>
                             <div class="levels" v-for="level in entry.createdLevels">
-                                <template v-if="level.rank <= 75">">
+                                <template v-if="level.rank <= 75">
                                     <strong>{{level.name}}</strong>
                                 </template>
                                 <template v-else>
