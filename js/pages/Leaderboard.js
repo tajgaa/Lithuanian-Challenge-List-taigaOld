2e47b7a87215944b846dd8baf757b85d81b38af3
KandyMun <rektelkid@gmail.com>
2024-08-26 15:41:22 +0300

    html update

--- js/pages/Leaderboard.js
+++ js/pages/Leaderboard.js
@@ -54,18 +54,14 @@ export default {
                             <!--Rank-->
                             <div class="extra">
                                 <h2 style="padding:5px; width:100%">Reitingas</h2>
-                                <div class="levels">
-                                    {{ selected + 1 }}
-                                </div>
-                            <div>
+                                <div class="levels">{{ selected + 1 }}</div>
+                            </div>
 
                             <!--Score-->
                             <div class="extra">
                                 <h2 style="padding:5px; width:100%">Taškai</h2>
-                                <div class="levels">
-                                    {{ entry.total }}\
-                                </div>
-                            <div>
+                                <div class="levels">{{ entry.total }}</div>
+                            </div>
 
                             <!--Hardest-->
                             <div class="extra">
@@ -76,14 +72,14 @@ export default {
                                 <template v-else>
                                     <div class="levels">Nieko</div>
                                 </template>
-                            <div>
+                            </div>
                         </div>
 
 
                         <!--Completed packs-->
                         <div class="stats-container">
                             <h2 style="padding:5px; width:100%">Įveikti pakeliai</h2>
-                            <template v-if="entry.packsComplete.length > 0">
+                            template v-if="entry.packsComplete.length > 0">
                                 <div class="packs">
                                     <div class="tag" v-for="pack in entry.packsComplete" :style="{background:pack.colour, color:getFontColour(pack.colour)}">
                                     {{pack.name}}
@@ -99,29 +95,29 @@ export default {
 
                         <!--Completed levels-->
                         <div class="stats-container">
-                        <h2 style="padding:5px; width:100%">Įveikti challenge'ai</h2>
-                        <template v-if="entry.completedLevels.length > 0">
-                            <div class="levels">
-                                <template v-for="(score, index) in entry.completedLevels">
-                                    <div class="level-display">
-                                        <template v-if="score.rank <= 75">
-                                            <a style="font-weight:bold;" :href="score.link">{{ score.level }}</a>
-                                        </template>
-                                        <template v-else>
-                                            <a style="font-style:italic; opacity: 60%;" :href="score.link">{{ score.level }}</a>
-                                        </template>
-                                    </div>
-                                    <div v-if="index !== entry.completedLevels.length - 1" class="level-display">-</div>
-                                </template
-                            </div>
-                        </template>
-                        <template v-else>
-                            <div class="levels">
-                                <div class="level-display" style="opacity:70%">Nieko</div>
-                            </div>
-                        </template>
+                            <h2 style="padding:5px; width:100%">Įveikti challenge'ai</h2>
+                            <template v-if="entry.completedLevels.length > 0">
+                                <div class="levels">
+                                    <template v-for="(score, index) in entry.completedLevels">
+                                        <div class="level-display">
+                                            <template v-if="score.rank <= 75">
+                                                <a style="font-weight:bold;" :href="score.link">{{ score.level }}</a>
+                                            </template>
+                                            <template v-else>
+                                                <a style="font-style:italic; opacity: 60%;" :href="score.link">{{ score.level }}</a>
+                                            </template>
+                                        </div>
+                                        <div v-if="index !== entry.completedLevels.length - 1" class="level-display">-</div>
+                                    </template>
+                                </div>
+                            </template>
+                            <template v-else>
+                                <div class="levels">
+                                    <div class="level-display" style="opacity:70%">Nieko</div>
+                                </div>
+                            </template>
                         </div>
-                        
+        
                         <div class="stats-container">
 
                             <!--Created levels-->
@@ -150,7 +146,7 @@ export default {
                             </div>
 
 
-                            <!--Verified levels-->
+                        <!--Verified levels-->
                             <div class="extra">
                                 <h2 style="padding:5px; width:100%">Patvirtinti challenge'ai</h2>
                                 <template v-if="entry.verifiedLevels.length > 0">
@@ -174,13 +170,12 @@ export default {
                                     </div>
                                 </template>
                             </div>
-
                         </div>
                     </div>
                 </div>
             </div>
         </main>
-    `,
+`,
     computed: {
         entry() {
             return this.leaderboard[this.selected];
